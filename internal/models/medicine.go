833219eba2d45996175df3a78a41d2821df45b77
dzhambazbiev-ux <dzhamb.azbiev@gmail.com>
2025-11-20 16:02:45 +0300

    добавил модель лекарств

---
+++ internal/models/medicine.go
@@ -0,0 +1,41 @@
+package models
+
+import "gorm.io/gorm"
+
+type Medicine struct {
+	gorm.Model
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Price                float64 `json:"price"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        int     `json:"stock_quantity"`
+	CategoryID           uint    `json:"category_id"`
+	SubcategoryID        uint    `json:"subcategory_id"`
+	Manufacturer         string  `json:"manufacturer"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `json:"avg_rating"`
+}
+
+type MedicineCreateRequest struct {
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Price                float64 `json:"price"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        int     `json:"stock_quantity"`
+	CategoryID           uint    `json:"category_id"`
+	SubcategoryID        uint    `json:"subcategory_id"`
+	Manufacturer         string  `json:"manufacturer"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `json:"avg_rating"`
+}
+
+type MedicineUpdateRequest struct {
+	Name                 *string  `json:"name"`
+	Description          *string  `json:"description"`
+	Price                *float64 `json:"price"`
+	InStock              *bool    `json:"in_stock"`
+	StockQuantity        *int     `json:"stock_quantity"`
+	Manufacturer         *string  `json:"manufacturer"`
+	PrescriptionRequired *bool    `json:"prescription_required"`
+	AvgRating            *float64 `json:"avg_rating"`
+}
