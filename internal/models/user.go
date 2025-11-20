e0182cd1cb36084581cc070b9c53ba1734686a47
usman <dzakaev.94@mail.ru>
2025-11-20 16:26:54 +0300

    добавил поля Cart, Orders и Reviews в модель User

--- internal/models/user.go
+++ internal/models/user.go
@@ -8,6 +8,9 @@ type User struct {
 	Email          string `json:"email"`
 	Phone          int    `json:"phone"`
 	DefaultAddress string `json:"default_address"`
+	Cart           Cart
+	Orders         []Order
+	Reviews        []Review
 }
 
 type UserCreateRequest struct {
