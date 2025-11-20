58bfc249db4d790e966affb83ba7a638ca9d8adb
rigel94 <legendalegenda20@gmail.com>
2025-11-20 16:02:49 +0300

    +reviews

---
+++ internal/models/review.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Review struct {
+	gorm.Model
+	UserID     uint   `json:"user_id"`
+	MedicineID uint   `json:"medicine_id"`
+	Rating     uint   `json:"rating"`
+	Text       string `json:"text"`
+}
+
+type ReviewForPost struct {
+	UserID     uint   `json:"user_id"`
+	MedicineID uint   `json:"medicine_id"`
+	Rating     uint   `json:"rating"`
+	Text       string `json:"text"`
+}
+
+type ReviewForUpdate struct {
+	UserID     *uint   `json:"user_id"`
+	MedicineID *uint   `json:"medicine_id"`
+	Rating     *uint   `json:"rating"`
+	Text       *string `json:"text"`
+}
