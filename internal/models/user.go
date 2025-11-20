f4d7b55ab688bb51ab1da8edd8a53ac6fbee8e1f
usman <dzakaev.94@mail.ru>
2025-11-20 15:49:34 +0300

    добавил модель User

---
+++ internal/models/user.go
@@ -0,0 +1,25 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	FullName       string `json:"full_name"`
+	Email          string `json:"email"`
+	Phone          int    `json:"phone"`
+	DefaultAddress string `json:"default_address"`
+}
+
+type UserCreateRequest struct {
+	FullName       string `json:"full_name"`
+	Email          string `json:"email"`
+	Phone          int    `json:"phone"`
+	DefaultAddress string `json:"default_address"`
+}
+
+type UserUpdateRequest struct {
+	FullName       *string `json:"full_name"`
+	Email          *string `json:"email"`
+	Phone          *int    `json:"phone"`
+	DefaultAddress *string `json:"default_address"`
+}
