bc768cf56caedcc1051760d01fa59ffbb6a782fa
Kevin <legendalegenda20@gmail.com>
2025-11-24 16:03:35 +0300

    ура все закончил (#28)

---
+++ internal/transport/review.handler.go
@@ -0,0 +1,147 @@
+package transport
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kuduzow/team-4-pharmacy/internal/models"
+	"github.com/kuduzow/team-4-pharmacy/internal/services"
+)
+
+type ReviewHandler struct {
+	service services.ModelService
+}
+
+func NewReviewHandler(service services.ModelService) *ReviewHandler {
+	return &ReviewHandler{service: service}
+}
+
+func (h *ReviewHandler) CreateReview(c *gin.Context) {
+	var req models.ReviewForPost
+
+	medicineID := c.Param("id")
+
+	convert, err := strconv.ParseUint(medicineID, 10, 64)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	req.MedicineID = uint(convert)
+
+	review, err := h.service.CreateReview(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, review)
+
+}
+
+func (h *ReviewHandler) DeleteReview(c *gin.Context) {
+	id := c.Param("id")
+
+	convert, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = h.service.DeleteReview(uint(convert))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "deleted"})
+
+}
+
+func (h *ReviewHandler) UpdateReview(c *gin.Context) {
+	var input models.ReviewForUpdate
+	id := c.Param("id")
+
+	convert, err := strconv.ParseUint(id, 10, 64)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	review, err := h.service.UpdateReview(uint(convert), input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, review)
+
+}
+
+func (h *ReviewHandler) GetByID(c *gin.Context) {
+
+	id := c.Param("id")
+
+	convert, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	review, err := h.service.GetByID(uint(convert))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, review)
+
+}
+
+func (h *ReviewHandler) ListByMedicineID(c *gin.Context) {
+	id := c.Param("id")
+
+	medicineID, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
+
+	reviews, err := h.service.ListByMedicineID(uint(medicineID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, reviews)
+}
+
+func (h *ReviewHandler) GetAvgRating(c *gin.Context) {
+	id := c.Param("id")
+
+	medicineID, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
+
+	reviews, err := h.service.GetAvgRating(uint(medicineID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, reviews)
+}
